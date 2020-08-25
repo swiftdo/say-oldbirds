cb0ecba5494d2afbd8f4cb9250758a6fee29ce8a
jihua <1164258202@qq.com>
2020-08-25 22:12:06 +0800

    Feat: 完成初版功能

--- Sources/App/Controllers/MessageController.swift
+++ Sources/App/Controllers/MessageController.swift
@@ -5,29 +5,32 @@ import Leaf
 struct MessageController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         routes.get(use: index)
-        routes.post(use: create)
+        routes.post("create", use: create)
     }
 
     func index(req: Request) throws -> EventLoopFuture<View> {
         struct Context: Encodable {
             let messages: [Message.ViewContext]
+            let count: Int
         }
         return Message
             .query(on: req.db)
             .sort(\.$createdAt, .descending)
             .all()
             .flatMapThrowing {
-                return try $0.map{ try Message.ViewContext(message: $0) }
+                return try $0.map{ try Message.ViewContext(message: $0)}
             }
             .flatMap {
-
-                let context = Context(messages: $0)
+                let context = Context(messages: $0, count: $0.count)
                 return req.view.render("index", context)
             }
     }
 
-    func create(req: Request) throws -> EventLoopFuture<Message> {
+    func create(req: Request) throws ->  EventLoopFuture<Response>{
         let message = try req.content.decode(Message.self)
-        return message.save(on: req.db).map { message }
+        req.logger.log(level: .info, "\(message)")
+        return message.save(on: req.db).map {
+            return req.redirect(to: "/")
+        }
     }
 }
