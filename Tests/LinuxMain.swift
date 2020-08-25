45eb5ed3607179d628c2a1b317ee8d691525cbe6
jihua <1164258202@qq.com>
2020-08-25 23:19:34 +0800

    adding heroku build files

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by laijihua on 2020/8/25.
+//
+
+import XCTest
+@testable import MonoGeneratorTests
+
+XCTMain([
+     testCase(GeneratorTests.allTests),
+])
